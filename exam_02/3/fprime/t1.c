1e7f0a1d9b621a8b0240c73c06734d569a6b5ef6
blueyaGIT <albanodavide50@gmail.com>
2025-01-29 12:26:28 +0100

    up

--- exam_02/3/fprime/t1.c
+++ exam_02/3/fprime/t1.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-void print_prime_factors(int n) {
-    int i = 2;
+void fprime(int n)
+{
+	int i = 2;
 	int first = 1;
 
-	while (n > i)
+	while (n > 1)
 	{
 		while (n % i == 0)
 		{
@@ -17,21 +18,14 @@ void print_prime_factors(int n) {
 		}
 		i++;
 	}
-	printf("\n");
 }
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        printf("\n");
-        return 1;
-    }
-
-    int n = atoi(argv[1]);
-    if (n > 0) {
-        print_prime_factors(n);
-    } else {
-        printf("\n");
-    }
-
-    return 0;
+int main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		fprime(atoi(av[1]));
+	}
+	printf("\n");
+	return (0);
 }
