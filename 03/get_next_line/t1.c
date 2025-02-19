c003f0c1606032e0dae03009e4413b32e79d5124
blueyaGIT <albanodavide50@gmail.com>
2025-02-19 17:34:06 +0100

    up

--- 03/get_next_line/t1.c
+++ 03/get_next_line/t1.c
@@ -1,5 +1,5 @@
-#ifndef BUFFER
-#define BUFFER 42
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 42
 #endif
 
 #include <unistd.h>
@@ -27,17 +27,17 @@ char *get_next_line(int fd)
 {
 	static int pos;
 	static int rd;
-	static char buf[BUFFER];
+	static char buf[BUFFER_SIZE];
 	int i = 0;
 	char line[70000];
 
-	if (fd < 0 || BUFFER <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return NULL;
 	while (1)
 	{
 		if (pos >= rd)
 		{
-			rd = read(fd, buf, BUFFER);
+			rd = read(fd, buf, BUFFER_SIZE);
 			pos = 0;
 			if (rd <= 0)
 				break;
